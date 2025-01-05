72f8c601880c16ee0a03e883d110004bb814fbf6
Jorge Poignon Garicano <jpoignon@uoc.edu>
2025-01-05 20:55:04 +0100

    Add Npc character

---
+++ Assets/NpcController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NpcController : MonoBehaviour
+{
+    // Reference to the player GameObject
+    GameObject player;
+    // Reference to the PlayerController script
+    PlayerController playerController;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.Log("No player found in the scene, make sure player GameObject has tag 'Player' assigned");
+        }
+        else
+        {
+            playerController = player.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damageable damageable = collision.GetComponent<Damageable>();
+        if (damageable != null)
+        {
+            if (damageable.IsPlayer)
+            {
+                // Collision detected with player
+                playerController.Celebrate();
+                // Invoke function after 8s
+                Invoke("DelaySceneLoad", 8);
+            }
+        }
+    }
+
+    private void DelaySceneLoad()
+    {
+        SceneManager.LoadSceneAsync("MainMenuScene");
+    }
+}
